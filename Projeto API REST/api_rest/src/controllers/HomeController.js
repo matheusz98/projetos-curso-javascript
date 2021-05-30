218f08ba457ac0b262d956162072efd9965474bb
matheusz98 <matheuscastiglia31@gmail.com>
2021-05-30 03:55:00 -0300

    Rotas de alunos

--- Projeto API REST/api_rest/src/controllers/HomeController.js
+++ Projeto API REST/api_rest/src/controllers/HomeController.js
@@ -1,16 +1,6 @@
-import Aluno from '../models/Aluno';
-
 class HomeController {
   async index(req, res) {
-    const novoAluno = await Aluno.create({
-      nome: 'Guilherme',
-      sobrenome: 'Fidelis',
-      email: 'fidelisamigao@gmail.com',
-      idade: 20,
-      peso: 71,
-      altura: 1.7,
-    });
-    res.json(novoAluno);
+    res.json('Index');
   }
 }
 
